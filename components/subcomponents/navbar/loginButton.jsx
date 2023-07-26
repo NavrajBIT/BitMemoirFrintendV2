c55deb076d456bcb9add51957737b55a176cf11e
MaahiVohra <vohramaahi@gmail.com>
2023-07-26 17:02:51 +0530

    ui: signup and getwallet btns

--- components/subcomponents/navbar/loginButton.jsx
+++ components/subcomponents/navbar/loginButton.jsx
@@ -4,15 +4,39 @@ import Button from "../button/button";
 import { useRouter } from "next/navigation";
 
 const LoginButton = () => {
-  const router = useRouter();
-  return (
-    <Button
-      text="Login"
-      variant="primary"
-      endIcon="login"
-      onClick={() => router.push("login")}
-    />
-  );
+	const router = useRouter();
+	return (
+		<div
+			style={{
+				alignItems: "center",
+				display: "flex",
+				gap: "1em",
+				justifyContent: "center",
+				margin: "2em 1em",
+			}}>
+			<Button
+				text={"Sign Up"}
+				style={{
+					color: "var(--white-100)",
+					fontSize: "1.25rem",
+					padding: "0.5em 1.2em",
+					border: "1px solid var(--primary-60)",
+					borderRadius: "0.3em",
+				}}
+			/>
+			<Button
+				text={"Get Wallet"}
+				style={{
+					color: "var(--white-100)",
+					fontSize: "1.25rem",
+					padding: "0.5em 1em",
+					border: "1px solid var(--primary-60)",
+					borderRadius: "0.3em",
+					backgroundColor: "var(--primary-60)",
+				}}
+			/>
+		</div>
+	);
 };
 
 export default LoginButton;
