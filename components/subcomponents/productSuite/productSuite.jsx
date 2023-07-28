847b89f1d13037521aa80ceb7dbf6a8108c2f21e
MaahiVohra <vohramaahi@gmail.com>
2023-07-28 15:20:34 +0530

    ui done

--- components/subcomponents/productSuite/productSuite.jsx
+++ components/subcomponents/productSuite/productSuite.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import "./productSuite.css";
 const ProductSuite = () => {
 	const products = [
 		"Authentication and Verification",
@@ -14,6 +14,7 @@ const ProductSuite = () => {
 	];
 	return (
 		<section
+			className="productSuiteContainer"
 			style={{
 				display: "flex",
 				alignItems: "center",
@@ -23,27 +24,70 @@ const ProductSuite = () => {
 				marginBottom: "4rem",
 			}}>
 			<div
+				className="productSuiteBackground"
 				style={{
-					// backgroundImage:
-					// 	"linear-gradient(to bottom,rgba(15, 48, 62, 1),rgba(15, 48, 62, 0), rgba(15, 48, 62, 1)),url('/assets/images/background_4.svg')",
-					background: "url('/assets/images/background_4.svg')",
 					backgroundRepeat: "no-repeat",
-					backgroundSize: "cover",
+					backgroundSize: "contain",
+					backgroundPosition: "center",
 					mixBlendMode: "overlay",
 					height: "100%",
 					width: "100%",
 					position: "absolute",
-					top: "0",
+					top: "10%",
+					left: "50%",
+					transform: "translate(-50%)",
 					zIndex: "-1",
-				}}></div>
+					display: "flex",
+					justifyContent: "center",
+					margin: "auto",
+					background: "url('/assets/images/background_4.svg')",
+				}}>
+				<Image
+					src={"/assets/images/productSuiteBackgroundImage.png"}
+					fill
+					alt="productSuiteBackground"
+					className="productSuiteBackgroundImage"
+					style={{
+						display: "none",
+					}}
+				/>
+			</div>
 			<div
 				style={{
-					marginTop: "4rem",
+					marginTop: "8rem",
 					fontSize: "1.5rem",
 					fontWeight: "700",
+					gridColumn: "1/4",
+					gridRow: "1/2",
+					textAlign: "center",
 				}}>
 				Product Suite
 			</div>
+			<div
+				className="productSuiteDesktop"
+				style={{ width: "100%", display: "none", gridRow: "2/3" }}>
+				{products.slice(0, 3).map((product, index) => {
+					return (
+						<div
+							key={index}
+							style={{
+								backgroundColor: "var(--primary-100)",
+								padding: "1.5rem",
+								borderRadius: "0.5rem",
+								width: "10rem",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								textAlign: "center",
+								margin: "1.5rem auto",
+								height: "8rem",
+							}}>
+							{product}
+						</div>
+					);
+				})}
+			</div>
+
 			<div
 				style={{
 					backgroundColor: "var(--white-100)",
@@ -51,9 +95,11 @@ const ProductSuite = () => {
 					height: "242px",
 					borderRadius: "50%",
 					display: "flex",
+					flexDirection: "column",
 					alignItems: "center",
 					justifyContent: "center",
-					marginTop: "4rem",
+					gridRow: "2/3",
+					margin: "auto",
 				}}>
 				<Image
 					height={300}
@@ -62,7 +108,66 @@ const ProductSuite = () => {
 					alt="productSuite"
 				/>
 			</div>
+
 			<div
+				className="productSuiteDesktop"
+				style={{
+					maxWidth: "100%",
+					display: "none",
+					gridRow: "2/3",
+				}}>
+				{products.slice(3, 6).map((product, index) => {
+					return (
+						<div
+							key={index}
+							style={{
+								backgroundColor: "var(--primary-100)",
+								padding: "1.5rem",
+								borderRadius: "0.5rem",
+								width: "10rem",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								textAlign: "center",
+								margin: "1.5rem auto",
+								height: "8rem",
+							}}>
+							{product}
+						</div>
+					);
+				})}
+			</div>
+			<div
+				className="productSuiteDesktopLower productSuiteDesktop"
+				style={{
+					maxWidth: "100%",
+					display: "none",
+					gridColumn: "1/4",
+					gridRow: "3/4",
+				}}>
+				{products.slice(6, 9).map((product, index) => {
+					return (
+						<div
+							key={index}
+							style={{
+								backgroundColor: "var(--primary-100)",
+								padding: "1.5rem",
+								borderRadius: "0.5rem",
+								width: "10rem",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								textAlign: "center",
+								margin: "auto",
+								height: "8rem",
+							}}>
+							{product}
+						</div>
+					);
+				})}
+			</div>
+			<div
+				className="productSuiteMobile"
 				style={{
 					display: "flex",
 					flexWrap: "wrap",
