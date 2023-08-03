b3f773ee70c6b40ebbf6ce436b60e4c3af65bbcf
vickyimagine <vivek@beimagine.tech>
2023-08-03 12:06:26 +0530

    Paging is added with some background changes

--- components/blogs/blogDescrp.jsx
+++ components/blogs/blogDescrp.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 import { IoArrowBackCircle } from "react-icons/io5";
 const BlogDescrp = ({ image, title, data, setExpanded }) => {
-    return (<div>
+    return (
+        <div>
+            <div className="max-w-screen-lg mx-auto">
+                <main className="mt-10">
+                    <button
+                        onClick={() => {
+                            setExpanded(false);
+                        }}
+                        className="text-3xl mb-7 flex gap-3"
+                    >
+                        <IoArrowBackCircle />
+                        <p className="text-xl font-semibold">Back</p>
+                    </button>
+                    <div className="mb-4 md:mb-0 w-full mx-auto relative">
+                        <div className="px-4 lg:px-0">
+                            <h2 className="text-4xl font-semibold text-white leading-tight">
+                                {title}
+                            </h2>
+                        </div>
 
-        <div className="max-w-screen-lg mx-auto">
-            <main className="mt-10">
-                <button onClick={() => {
-                    setExpanded(false)
-                }} className="text-3xl mb-7 flex gap-3"><IoArrowBackCircle /><p className="text-xl font-semibold">Back</p></button>
-                <div className="mb-4 md:mb-0 w-full mx-auto relative">
-                    <div className="px-4 lg:px-0">
-                        <h2 className="text-4xl font-semibold text-white leading-tight">
-                            {title}
-                        </h2>
-
+                        <img
+                            src="https://img.freepik.com/premium-photo/big-data-flow-blockchain-data-fields-network-ai-generated-art-work_844516-149.jpg"
+                            className="w-full h-[400px] lg:w-[1000px] lg:h-[400px] lg:rounded mx-auto mt-4 rounded-md"
+                            alt="Blog"
+                        />
                     </div>
 
-                    <img src="https://img.freepik.com/premium-photo/big-data-flow-blockchain-data-fields-network-ai-generated-art-work_844516-149.jpg" className="w-[1000px] h-[400px] lg:rounded mx-auto mt-4" />
-                </div>
-
-                <div className="flex flex-col lg:flex-row lg:space-x-12 w-screen">
-
-                    <div className="px-4 lg:px-0 mt-12 text-white-700 text-lg text-start leading-relaxed  lg:w-3/4 ">
-                        {data}
+                    <div className="flex flex-col lg:flex-row lg:space-x-12 w-full lg:max-w-screen-lg mx-auto">
+                        <div className="px-4 lg:px-0 mt-12 text-white-700 text-lg text-start leading-relaxed lg:w-3/4 overflow-wrap break-word">
+                            {data}
+                        </div>
                     </div>
-                </div>
-            </main>
-
+                </main>
+            </div>
         </div>
-
-    </div>)
+    );
 };
 
 export default BlogDescrp;
+
--- components/blogs/blogs.jsx
+++ components/blogs/blogs.jsx
@@ -1,55 +1,66 @@
 "use client"
-import { useState } from "react";
+
+import React, { useState } from "react";
 import CardBlock from "./cardBlock";
 import CardRaw from "./cardRaw";
-import "./blog.css"
 import BlogDescrp from "./blogDescrp";
 import { blogData } from "./blogData";
-const Blogs = () => {
 
-    const [isExpanded, setIsExpanded] = useState(false)
-    const [title, setTitle] = useState()
-    const [image, setImage] = useState()
-    const [data, setData] = useState()
+const Blogs = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [title, setTitle] = useState();
+    const [image, setImage] = useState();
+    const [data, setData] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
 
     const latestBlogs = blogData.slice(-6).reverse(); // Get the first 6 elements
     const remBlogs = blogData.slice(0, -6).reverse(); // Get the remaining elements
 
+    const totalPages = Math.ceil(remBlogs.length / itemsPerPage);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     const test = () => {
-        console.log(isExpanded)
-    }
-    return (
-        isExpanded ? (<BlogDescrp setExpanded={setIsExpanded} title={title} image={image} data={data} />) :
-            (<>
-                <div className="px-4 sm:px-6 lg:px-8">
-                    <div className="flex flex-col justify-center items-center mt-10 gap-y-10">
-                        <div className="flex justify-between w-full">
-                            <p className="text-white text-xl font-semibold">Top Blogs</p>
-                            <div className="flex gap-4">
-                                <p className="border px-2 cursor-pointer rounded-sm">Sort</p>
-                                <p className="border px-2 cursor-pointer rounded-sm">Filter</p>
-                            </div>
-                        </div>
+        console.log(isExpanded);
+    };
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                            {latestBlogs.map((item, index) => (
-                                <CardBlock
-                                    key={index}
-                                    index={index}
-                                    title={item.title}
-                                    data={item.data}
-                                    img={item.img}
-                                    setTitle={setTitle}
-                                    setImage={setImage}
-                                    setData={setData}
-                                    setExpanded={setIsExpanded}
-                                />
-                            ))}
+    return isExpanded ? (
+        <BlogDescrp setExpanded={setIsExpanded} title={title} image={image} data={data} />
+    ) : (
+        <>
+            <div className="px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col justify-center items-center mt-10 gap-y-10">
+                    <div className="flex justify-between w-full">
+                        <p className="text-white text-xl font-semibold">Top Blogs</p>
+                        <div className="flex gap-4">
+                            <p className="border px-2 cursor-pointer rounded-sm">Sort</p>
+                            <p className="border px-2 cursor-pointer rounded-sm">Filter</p>
                         </div>
+                    </div>
 
-                        <div className="flex flex-col gap-y-5">
-                            {remBlogs.map((item, index) => (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                        {latestBlogs.map((item, index) => (
+                            <CardBlock
+                                key={index}
+                                index={index}
+                                title={item.title}
+                                data={item.data}
+                                img={item.img}
+                                setTitle={setTitle}
+                                setImage={setImage}
+                                setData={setData}
+                                setExpanded={setIsExpanded}
+                            />
+                        ))}
+                    </div>
+
+                    <div className="flex flex-col gap-y-5">
+                        {remBlogs
+                            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+                            .map((item, index) => (
                                 <CardRaw
                                     key={index}
                                     index={index}
@@ -62,11 +73,26 @@ const Blogs = () => {
                                     setData={setData}
                                 />
                             ))}
-                        </div>
                     </div>
+
+                    {totalPages > 1 && (
+                        <div className="flex gap-2 mt-4">
+                            {Array.from({ length: totalPages }, (_, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => handlePageChange(index + 1)}
+                                    className={`px-3 py-2 rounded ${currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-700"
+                                        }`}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))}
+                        </div>
+                    )}
                 </div>
-            </>)
-    )
+            </div>
+        </>
+    );
 };
 
 export default Blogs;
