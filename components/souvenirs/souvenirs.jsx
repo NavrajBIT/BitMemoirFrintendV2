16f52eb7a7720e211a858481b8971dccd12045f7
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-08-03 11:46:32 +0530

    Enable rendering of uploaded file

--- components/souvenirs/souvenirs.jsx
+++ components/souvenirs/souvenirs.jsx
@@ -1,8 +1,24 @@
+"use client"
+
+import { useState } from "react";
 import Input from "../subcomponents/form/input"
 import Button from "../subcomponents/button/button";
 import './souvenirs.css';
 
 const Souvenirs = () => {
+    const [uploadedImage, setUploadedImage] = useState(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        console.log(file);
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setUploadedImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
     return (
         <div style={{
             // height: '80vh',
@@ -22,7 +38,7 @@ const Souvenirs = () => {
                 alignItems: 'center',
                 borderRadius: '8px',
                 position: 'relative'
-            }} id="formContainer">
+            }}>
                 <h1 style={{
                     color: 'var(--primary-light)',
                     position: 'absolute',
@@ -31,7 +47,6 @@ const Souvenirs = () => {
                 }} className="formHead"
                 >Souvenirs</h1>
                 <div style={{
-                    // border: '1px dashed var(--primary-light)',
                     height: '20rem',
                     width: '90%',
                     display: 'flex',
@@ -40,13 +55,17 @@ const Souvenirs = () => {
                     justifyContent: 'flex-end',
                     padding: '1.5rem'
                 }} id="uploadImgDiv">
-                    <img src="/souvenirs/uploadImg.png" alt="" style={{ margin: '2rem 0' }} />
+                    {uploadedImage ? (
+                        <img src={uploadedImage} alt="" style={{ margin: '2rem 0' }} />
+                    ) : (
+                        <img src="/souvenirs/uploadImg.png" alt="" style={{ margin: '2rem 0' }} />
+                    )}
                     <label htmlFor="img" style={{
                         textDecoration: 'underline',
                         cursor: 'pointer'
                     }} id="uploadCertLabel">
                         Click to upload certificate
-                        <input type="file" id="img" name="img" accept="image/*" style={{ display: 'none' }} />
+                        <input type="file" id="img" name="img" accept="image/*" style={{ display: 'none' }} onChange={handleImageUpload} />
                     </label>
                     <div style={{ margin: '8px 0' }} className="drag">OR</div>
                     <div className="drag">Drag and drop image to upload</div>
